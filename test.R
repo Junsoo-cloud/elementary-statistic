3ca6224543033d3dac48a2f66d7b774dcf1dff3d
Junsoo-cloud <kfxsmt@naver.com>
2025-03-17 10:56:40 +0900

    Rstats

--- test.R
+++ test.R
@@ -1,8 +1,21 @@
-x <- 10
+# Use $ to approach column in table. 
+# plot has attribute that specify plot type.
+# can compare each data to bool.
 
-
-# no need to import packages
-y <- 20
-
-print(x)
+# download web data through source
+source("http://www.openintro.org/stat/data/arbuthnot.R")
+arbuthnot
+dim(arbuthnot)
+names(arbuthnot)
+arbuthnot$boys
+plot(x = arbuthnot$year, y = arbuthnot$girls)
+plot(x = arbuthnot$year, y = arbuthnot$girls, type = "l")
+?plot
+5218 + 4683
+arbuthnot$boys + arbuthnot$girls
+plot(arbuthnot$year, arbuthnot$boys + arbuthnot$girls, type = "l")
+5218 / 4683
+5218 / (5218 + 4683)
+arbuthnot$boys / (arbuthnot$boys + arbuthnot$girls)
+arbuthnot$boys > arbuthnot$girls
 
