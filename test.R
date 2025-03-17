c15bc7f95a0c04e2bcc3a2d98014060f9a525995
Junsoo-cloud <kfxsmt@naver.com>
2025-03-17 11:32:15 +0900

    Rstats

--- test.R
+++ test.R
@@ -2,14 +2,17 @@
 # plot has attribute that specify plot type.
 # can compare each data to bool.
 
+# Run independently 
+
 # download web data through source
 source("http://www.openintro.org/stat/data/arbuthnot.R")
 arbuthnot
-dim(arbuthnot)
-names(arbuthnot)
+dim(arbuthnot) # dimension
+names(arbuthnot) # coulmns(feature) name 
 arbuthnot$boys
 plot(x = arbuthnot$year, y = arbuthnot$girls)
-plot(x = arbuthnot$year, y = arbuthnot$girls, type = "l")
+plot(x = arbuthnot$year, y = arbuthnot$girls, type = "l", col="blue", cex=5) # type == line
+lines(x = arbuthnot$year, y = arbuthnot$boys, type = "l", col="red", cex=5) # type == line
 ?plot
 5218 + 4683
 arbuthnot$boys + arbuthnot$girls
